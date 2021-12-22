2790dce0482c2d22ee762e3d6c63cab2cf44717d
Juan Pablo Calvo <juan.calvo@pedidosya.com>
2021-12-22 18:10:29 -0300

    clean up storybook

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -1,11 +1,14 @@
 import { css } from "styled-components";
 
 const primary = css`
-  background-color: blue;
+  color: white;
+  background-color: #1ea7fd;
 `;
 
 const secondary = css`
-  background-color: green;
+  color: #333;
+  background-color: transparent;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
 `;
 
 const terciary = css`
@@ -13,6 +16,13 @@ const terciary = css`
 `;
 
 const style = css`
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
   .primary {
     ${primary}
   }
